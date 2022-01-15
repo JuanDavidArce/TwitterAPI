2c7027df932e8368e0089ba2400c83a569e361a9
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-01-15 09:17:27 -0500

    users_signup_docstrign

--- main.py
+++ main.py
@@ -60,6 +60,14 @@ class Tweet(BaseModel):
     by: User = Field(...)
 
 
+class UserRegister(User):
+    password:str = Field(
+        ...,
+        min_length=8,
+        max_length=50
+    )
+
+
 # Path Operations
 
 
@@ -74,7 +82,23 @@ class Tweet(BaseModel):
     tags=["Users"]
 )
 def signup():
-    pass
+    """
+    Signup
+
+    This path operation register a user in the app
+
+
+    Parameters:
+        -Request body parameter
+            - user : UserRegister
+    
+    Returns a json with the basic user information:
+        - user_id : UUID
+        - email: Emailstr
+        - first_name : str
+        - last_name : str
+        - birth_date : str
+    """
 
 
 ### Login a user
