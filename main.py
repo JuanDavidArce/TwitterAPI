fdd0286c00902fdd785b81c870900193d47177ec
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-01-15 10:00:55 -0500

    users_show_all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Python
 import json
 from typing import Optional, List
+from unittest import result
 from uuid import UUID
 from datetime import date
 from datetime import datetime
@@ -140,7 +141,29 @@ def login():
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all users in the app
+
+    Parameters:
+
+        -
+
+    Returns a json list with all users in the app with the following keys:
+        
+        - user_id : UUID
+        
+        - email: Emailstr
+        
+        - first_name : str
+        
+        - last_name : str
+        
+        - birth_date : date
+    """
+
+    with open("users.json","r",encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 
 # Show a user
