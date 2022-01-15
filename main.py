6eab4438979cc4293bfcb71a5d5046669adaf5e1
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-01-15 08:43:02 -0500

    "models_tweets"

---
+++ main.py
@@ -0,0 +1,65 @@
+# Python
+from typing import Optional
+from uuid import UUID
+from datetime import date
+from datetime import datetime
+
+
+# Pydantic
+from  pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+
+# FastAPI
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+# Models
+class UserBase(BaseModel):
+    user_id:UUID=Field(...)
+    email:EmailStr=Field(...)
+
+
+class UserLogin(UserBase):
+    password:str = Field(
+        ...,
+        min_length=8,
+        max_length=50
+    )
+
+
+class User(UserBase):
+    first_name:str =Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+
+    last_name:str =Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date:Optional[date]=Field(default=None)
+
+
+class Tweet(BaseModel):
+    tweet_id:UUID = Field(...)
+    content: str = Field(
+        ...,
+        max_length=256,
+        min_length=1,
+        )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
+
+
+
+@app.get(path="/")
+def home():
+    return {"twitter API":"Working"}
